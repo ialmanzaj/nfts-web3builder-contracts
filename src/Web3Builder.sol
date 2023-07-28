98441b8e26bec4c95ad1da8ab90ae5ef24e26583
Isaac Almanza <isaac.almanza19@gmail.com>
2023-07-28 13:03:16 -0500

    add base for web3builder contract

---
+++ src/Web3Builder.sol
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract Web3Builder is ERC721, ERC721Enumerable, Pausable, Ownable {
+    using Counters for Counters.Counter;
+
+    uint maxSupply = 999;
+    bool public allowListMintOpen = false;
+    bool public allowListPublicMintOpen = false;
+    mapping(address => bool) public reservedList;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("Web3Builder", "WEB3") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://QmY5rPqGTN1rZxMQg2ApiSZc7JiBNs1ryDzXPZpQhC1ibm/";
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function editMintWindows(
+        bool _allowListMintOpen, 
+        bool _allowListPublicMintOpen) external onlyOwner{
+        allowListMintOpen = _allowListMintOpen;
+        allowListPublicMintOpen = _allowListPublicMintOpen;
+    }
+
+    // add Payment
+    // add limiting of supply
+    function publicMint() public  payable {
+        require(allowListPublicMintOpen, "Not open the public mint");
+        internalMint();
+    }
+
+    function setAllowList(address[] calldata addresses) external onlyOwner {
+        for(uint i; i< addresses.length;i++) {
+            reservedList[addresses[i]] = true;
+        }
+    }
+
+    // add Payment
+    // add limiting of supply
+    function allowListMint() public  payable {
+        require(allowListMintOpen, "Not open the public mint");
+        require(reservedList[msg.sender], "You are not in the VIP list");
+        internalMint();
+    }
+
+    function internalMint() internal  {
+        require(msg.value == 0.01 ether, "Not enough funds");
+        require(totalSupply() < maxSupply, "We sold out");
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(msg.sender, tokenId);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
+        internal
+        whenNotPaused
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
